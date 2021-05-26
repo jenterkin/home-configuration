2c994525bc95af6cadaf1ed9018c3e773e00c317
Jordan Enterkin <jordan.enterkin@jumpcloud.com>
2021-05-26 17:10:34 -0600

    move lsp config to plugin conf

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,17 +29,4 @@ augroup END
 vim.api.nvim_set_option('timeoutlen', 0)
 vim.g.mapleader = ','
 
--- require global lsp integrations
-g.capabilities = vim.lsp.protocol.make_client_capabilities()
-g.capabilities.textDocument.completion.completionItem.snippetSupport = true
-g.capabilities.textDocument.completion.completionItem.resolveSupport = {
-  properties = {
-    'documentation',
-    'detail',
-    'additionalTextEdits',
-  }
-}
-
-require('lspconfig').bashls.setup{capabilities = g.capabilities}
-
 vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', {noremap = true})
--- nvim/lua/conf/lspconfig-conf.lua
+++ nvim/lua/conf/lspconfig-conf.lua
@@ -2,5 +2,18 @@ require('packer').use {
     'neovim/nvim-lspconfig',
     config = function()
         vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+        -- set up global lsp integrations
+        vim.g.capabilities = vim.lsp.protocol.make_client_capabilities()
+        vim.g.capabilities.textDocument.completion.completionItem.snippetSupport = true
+        vim.g.capabilities.textDocument.completion.completionItem.resolveSupport = {
+            properties = {
+                'documentation',
+                'detail',
+                'additionalTextEdits',
+            }
+        }
+
+        require('lspconfig').bashls.setup{capabilities = vim.g.capabilities}
     end
 }
