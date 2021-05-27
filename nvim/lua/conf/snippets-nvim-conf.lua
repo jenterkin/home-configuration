545a9fae357774b5c04b1652353d8389e66399fb
Jordan Enterkin <jordan.enterkin@jumpcloud.com>
2021-05-26 18:06:37 -0600

    add snippets

--- nvim/lua/conf/snippets-nvim-conf.lua
+++ nvim/lua/conf/snippets-nvim-conf.lua
@@ -11,12 +11,25 @@ require('packer').use {
 
 function define_snippets()
     local snippets = require('snippets')
+    local util = require('snippets.utils')
     snippets.use_suggested_mappings()
 
     -- Define snippets
     snippets.snippets = {
         _global = {
             todo = "TODO(jenterkin): "
+        },
+        lua = {
+            ["for"] = util.match_indentation [[
+for ${1}, ${2} in ipairs(${3}) do
+    $0
+end]]
+        },
+        python = {
+            ["def"] = util.match_indentation [[
+def ${1}(${2}):
+    $0]],
+            ["print"] = "print(${0})"
         }
     }
 end
